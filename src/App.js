88b79f07542541d72d8b7ea0d7ae4be809e3c693
mwang840 <maxwang@udel.edu>
2024-03-02 15:34:23 -0500

    text blank done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Typings from './components/Typings';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>
+      <h1 className="AppName">
           TetraTyper
       </h1>
-      <h2>
-        The AI Typing Helper
-      </h2>
+      <h3>
+        The AI Typing Helper.
+      </h3>
+      <Typings></Typings>
         <img src={logo} className="App-logo" alt="logo" />
         
         <a
