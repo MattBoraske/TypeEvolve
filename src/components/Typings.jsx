d6a1309f0c1d76e20ced1a7a5e6a8b023ffce211
mwang840 <maxwang@udel.edu>
2024-03-02 17:54:02 -0500

    stash change

--- src/components/Typings.jsx
+++ src/components/Typings.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from "react-bootstrap/Form";
 import "./Typings.css";
 
 const Typings = () =>{
     
+    const [text, setText] = useState("");
+
+    function updateText(event) {
+        setText(event.target.value)
+    }
+
+
     return <Form className="typingForm">
         <Form.Group controlId="formBasicEmail">
-        <Form.Control type="text" placeholder="" />
+        <Form.Control type="text" placeholder="" value={text} onChange={updateText} />
       </Form.Group>
     </Form>
 };
