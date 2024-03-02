0922e7d55b80a067853a1b44bbe81a51e1062f37
connorsaunders <connorsaunders18@gmail.com>
2024-03-02 18:32:55 -0500

    Timer implementation

--- src/components/parent-component/parent-component.js
+++ src/components/parent-component/parent-component.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import Typings from '../typing-box/Typings';
-import TextBox from '../target-typing-box/target';
+import TextBox from '../target-typing-box/Target';
 
 const ParentComponent = () => {
   // State to hold the input value
--- src/components/target-typing-box/target.js
+++ src/components/target-typing-box/target.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './target.css';
 
-const TextBox = ({ text }) => { // Destructure the text prop
+const TextBox = ({ text }) => {
+  ////////////////////////////////////////////////////////////////
+  // The target text to type goes here:
+  /////////////////////////////////////////////////////////////////
+  const targetText = "This is the target text"; 
+
+  ////////////////////////////////////////////////////////////////
+  // Set variables:
+  /////////////////////////////////////////////////////////////////
+  const [startTime, setStartTime] = useState(null);
+  const [endTime, setEndTime] = useState(null);
+  const [timerRunning, setTimerRunning] = useState(false);
+
+  ////////////////////////////////////////////////////////////////
+  // Timer starts when textbox length is 1
+  /////////////////////////////////////////////////////////////////
+  useEffect(() => {
+    if (text.length === 1 && !timerRunning) {
+      setStartTime(new Date());
+      setTimerRunning(true);
+    }
+
+  ////////////////////////////////////////////////////////////////
+  // Stop timer when text matches target text
+  /////////////////////////////////////////////////////////////////
+    if (text === targetText && timerRunning) {
+      setEndTime(new Date());
+      setTimerRunning(false);
+    }
+  }, [text, timerRunning, targetText]); // Dependencies for useEffect
+
+  ////////////////////////////////////////////////////////////////
+  // Get timer diff:
+  /////////////////////////////////////////////////////////////////
+  const elapsedTime = endTime ? ((endTime - startTime) / 1000).toFixed(2) : 0;
 
   return (
-    <><div className='text-box-to-type-to'>
-      This is the target text
-    </div><div className="text-box">
+    <>
+      <div className='text-box-to-type-to'>
+        {targetText}
+      </div>
+      <div className="text-box">
         {text}
-      </div></>
+      </div>
+      {/* Optionally display the timer or completion message */}
+     {!timerRunning && endTime && (
+        <div>Time taken: {elapsedTime} seconds</div>
+      )}
+    </>
   );
 };
 
