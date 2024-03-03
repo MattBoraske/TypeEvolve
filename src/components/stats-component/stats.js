2d818af6b9999d8cb8501930198c6caa9bb4ff50
connorsaunders <connorsaunders18@gmail.com>
2024-03-03 08:40:26 -0500

    Curs

--- src/components/stats-component/stats.js
+++ src/components/stats-component/stats.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 const StatsDisplay = ({ realComparisonResults, text, elapsedTime }) => {
   return (
-    <div>
+    <div className="stats-container">
       <h3>Results</h3>
-      <p>Accuracy: {realComparisonResults.accuracy}%</p>
-      <p>WPM: {(text.split(" ").length / (elapsedTime / 60)).toFixed(0)}</p>
-      <p>Missed words: {realComparisonResults.missedWords.join(", ")}</p>
-      <p>Error characters: {realComparisonResults.errorCharacters.join(", ")}</p>
-      <p>Elapsed time: {elapsedTime} seconds</p>
+      <div className="stats-row">
+        <div className="stats-column">
+          <p><strong>Accuracy:</strong> {realComparisonResults.accuracy}%</p>
+          <p><strong>WPM:</strong> {(text.split(" ").length / (elapsedTime / 60)).toFixed(0)}</p>
+        </div>
+        <div className="stats-column">
+          <p><strong>Missed words:</strong> {realComparisonResults.missedWords.join(", ")}</p>
+          <p><strong>Error characters:</strong> {realComparisonResults.errorCharacters.join(", ")}</p>
+          <p><strong>Elapsed time:</strong> {elapsedTime} seconds</p>
+        </div>
+      </div>
     </div>
   );
 };
