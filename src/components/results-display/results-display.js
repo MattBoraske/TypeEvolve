6e75b490f43a1681472190192d60e662c95c7e25
connorsaunders <connorsaunders18@gmail.com>
2024-03-02 22:44:51 -0500

    Pieces

--- src/components/results-display/results-display.js
+++ src/components/results-display/results-display.js
@@ -5,18 +5,16 @@ const ResultsDisplay = ({ elapsedTime, targetText, text, comparisonResults }) =>
   return (
     <div className="results-display">
       <h3>Results</h3>
-      <p>Time taken: {elapsedTime} seconds</p>
-      <p>Target: {targetText}</p>
-      <p>Actual: {text}</p>
       {comparisonResults && (
         <div>
           <p>Accuracy: {comparisonResults.accuracy}%</p>
           <p>Missed words: {comparisonResults.missedWords.join(", ")}</p>
           <p>Error characters: {comparisonResults.errorCharacters.join(", ")}</p>
+          ,<p>Elapsed time: {elapsedTime} seconds</p>
+          <h3>Keyboard Heatmap</h3>
+        <Keyboard />
         </div>
       )}
-      <h3>Keyboard Heatmap</h3>
-      <Keyboard />
     </div>
   );
 };
